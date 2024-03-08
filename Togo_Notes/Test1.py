54092e891f58fb983684ba06712311efd5f57cfa
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2024-03-08 14:50:49 -0500

    Create Test1.py

---
+++ Togo_Notes/Test1.py
@@ -0,0 +1,21 @@
+Import sys
+
+dir(sys)
+
+help(sys.getsizeof)
+
+Spark do it in memory, Hadoop has to read from disk and write to disk
+
+https://colab.research.google.com
+
+Sample Data
+https://lnkd.in/g-GxEYV
+
+GitHub:main > Apache_Pyspark_by_Example.ipynb
+
+https://lnkd.in/g-GxEYV
+
+
+--Working Dataframe API
+Dataframes are High_level API
+Resilient Distributed Datasets(RDD's) - Low-level API
