92d7f358ad816709e5bd721e697a715d66cfe008
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2024-04-30 16:12:31 -0400

    Create Stack.py

---
+++ DataStructures/Stack.py
@@ -0,0 +1,11 @@
+--Stack
+
+A stack is a linear data structure in which elements can be inserted and deleted only from one side of the list, called the top. A stack follows the LIFO (Last In First Out) principle, i.e., the element inserted at the last is the first element to come out.
+
+Stack is an object to store the data in a very specific way. The alternative name for a stack (but only in IT terminology) is LIFO.
+
+--Operations possible on the stack are:
+
+ Push or insert an element to the top of the stack
+ Pop or remove an element from top of the stack
+
