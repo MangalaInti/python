a0f3d41436cdbe9ff197cfe0408d53cffe0be21a
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2024-04-30 16:13:30 -0400

    Create ex-1.py

---
+++ DataStructures/Stack/ex-1.py
@@ -0,0 +1,3 @@
+Write a program to represent a Stack class as discussed earlier and implement push(data) and is_full() methods to implement the push operation based on the algorithm discussed.
+
+Once done, represent Peter’s shirts as a stack of maximum size 5 and push the following shirts into it.
