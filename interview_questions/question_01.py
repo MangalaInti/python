ebddbd8a2531ae4c804502151674b374686f1dc9
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2024-06-04 18:25:36 -0400

    Update question_01.py

--- interview_questions/question_01.py
+++ interview_questions/question_01.py
@@ -26,6 +26,65 @@ values= ['one','Two','Three']
 result = dict(zip(keys,values))
 print(result)
 
+#Sort a List in Descending Order
+lst = [2, 5, 6, 8, 1, 8, 9, 11]
+lst.sort(reverse = True)
+print(lst)
+
+
+#Count item occurence
+list1 = ['John','Kelly', 'Peter', 'Moses', 'Peter']
+count = 0
+for i in list1:
+    if i == 'Peter':
+     count = count+1
+print(count)
+
+#Alternate way
+list1.count('Peter')
+
+Python has a built in module called calender
+import calender
+
+How to get current date and time
+from datetime import datetime
+
+#Flatten a nested list
+list1 = [[1, 2, 3],[4, 5, 6]]
+new_lst = []
+for i in list1:
+  for j in i:
+    new_lst.append(j)     
+print(new_lst) 
+
+#Find the index of Biggest number
+
+def index_of_largest(lst):
+    if not lst:
+        return None  # Return None if the list is empty
+    max_index = 0
+    for i in range(1, len(lst)):
+        if lst[i] > lst[max_index]:
+           max_index = i
+    return max_index
+
+# Example usage:
+numbers = [10, 5, 20, 8, 15]
+largest_index = index_of_largest(numbers)
+print("Index of largest number:", largest_index)
+print("Largest number:", numbers[largest_index])
+
+# Empty string
+str1 = ''
+if not str1:
+print('This string is empty')
+else:
+print('This string is NOT empty')
+Output:
+This string is empty
+Example
+
+
     
 
 
