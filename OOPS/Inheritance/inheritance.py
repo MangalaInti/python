43f45d56e39bb6b4a47d88ac52060b39e890e4c8
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2025-04-11 17:40:45 -0400

    Create inheritance.py

---
+++ OOPS/Inheritance/inheritance.py
@@ -0,0 +1,11 @@
+The process of inheriting the properties of the parent class into a child class is called inheritance. 
+The existing class is called a base class or parent class and 
+the new class is called a subclass or child class or derived class
+--Advanges
+ There are three main advantages of inheritance:
+•	Common properties can be kept in a single class, thus any changes need to be made need not be repeated
+•	Inheritance encourages code reuse thus saving us time
+
+  **To create an inheritance relationship between the classes, mention the name of the parent class in brackets
+  
+ 
