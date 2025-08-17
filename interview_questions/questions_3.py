12ad1094c082431348a6007ac71e383ab433a0b5
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2025-08-17 17:16:32 -0400

    Update questions_3.py

--- interview_questions/questions_3.py
+++ interview_questions/questions_3.py
@@ -2,3 +2,15 @@
 numbers = [3,2,5,2,7,3,1,8,5,4,6]
 
 list = sorted(set(x**2 for x in numbers), reverse = True)
+
+--Write a program to accept a string from the user and display characters that are present at an even index number.
+For example, str = "pynative" so you should display ‘p’, ‘n’, ‘t’, ‘v’.
+str = 'pynative'
+size = len(str)
+for i in range(0,size -1, 2):
+    print(str[i])
+--Alternate Way
+
+x = list(str)
+for i in x[0::2]:
+    print(i)
