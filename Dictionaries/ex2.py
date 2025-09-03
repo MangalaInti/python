935e69534b400c398abbaa36f65fce921ad587cd
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2025-09-02 20:03:41 -0400

    Update ex2.py

--- Dictionaries/ex2.py
+++ Dictionaries/ex2.py
@@ -1,3 +1,18 @@
+# Write code to Delete a list of keys from a dictionary
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+
+# Keys to remove
+keys_remove = ["name", "salary"]
+
+dict3 = {k:v for k, v in sample_dict.items() 
+          if k not in keys_remove}
+print(dict3)
+
 #Get the key of a minimum value from the following dictionary
 sample_dict = {
   'Physics': 82,
