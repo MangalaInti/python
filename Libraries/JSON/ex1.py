5ccfae3c0d22049b2d0b0c0d711d1587f8e27e7e
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2025-09-04 16:42:21 -0400

    Update ex1.py

--- Libraries/JSON/ex1.py
+++ Libraries/JSON/ex1.py
@@ -1,1 +1,19 @@
-bbb
+
+# conversion of dict vice versa 
+ import json
+ data = {"key1" : "value1", "key2" : "value2"}
+
+ res = json.dumps(data)            # convert dict to json string
+ parsed = json.loads(res)          # convert json string back to dict 
+ # To access any values in python we need to convert to dictionary
+
+print(parsed["key2"])
+
+# Readable format
+
+import json
+data = {"key1" : "value1", "key2" : "value2"}
+res = json.dumps(data, indent =2 separator = (",", "="))
+print(res)
+
+
