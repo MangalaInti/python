e9fb696c6bf5e66739b48a64bac212bc0cc88eea
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2025-09-04 15:04:52 -0400

    Update Json_Dict.py

--- Libraries/Json_Dict.py
+++ Libraries/Json_Dict.py
@@ -3,3 +3,29 @@ json.load() = > write to a file
 
 --You use json.loads() when your data is already in your python pgm
 
+----Function             Input Type           Typical Use case
+-----------------------------------------------------------------------
+  json.loads()        JSON string             When you have JSON as a string
+  json.load()         File Object             When reading JSON from a file 
+
+# Json  to python object
+import json
+
+json_str = '{"name" : "mangala", "role": "consultant"}'
+
+data = json.loads.(json_str)
+print(data["name"])    #mangala
+
+# Loading Json as File object
+
+import json
+
+with open('data.json', 'r') as f:
+  data = json.load(f)
+print(data["name"])
+
+
+
+
+
+
