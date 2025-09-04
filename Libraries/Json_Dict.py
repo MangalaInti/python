b14a3b221c86584cce2cac59ae22df94cacca8f9
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2025-09-04 14:44:59 -0400

    Update Json_Dict.py

--- Libraries/Json_Dict.py
+++ Libraries/Json_Dict.py
@@ -1,2 +1,5 @@
-json.loads
-json.lod
+json.loads() => Used to convert json data to python dictionary, Output is string
+json.load() = > write to a file
+
+--You use json.loads() when your data is already in your python pgm
+
