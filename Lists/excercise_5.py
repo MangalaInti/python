7fe79bc8cbc6f8e54d97cfb4cb22bcdbc7c21b4a
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2025-09-07 13:06:11 -0400

    Update excercise_5.py

--- Lists/excercise_5.py
+++ Lists/excercise_5.py
@@ -7,6 +7,23 @@ print(numbers)
 List comprehensions provide a concise way to create lists.
 isinstance() method to filter elements based on their type.
 
+ #concatenate two lists index wise
+
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+
+list3 = [ i+j for i,j in zip(list1,list2)]
+print(list3)
+
+# combine two lists in as expected o/p
+list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
+#Expected o/p
+list3 = [i +j for i in list1 for j in list2]
+print(list3)  
+
+     
+
 
 
 
