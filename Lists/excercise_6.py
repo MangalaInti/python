843b1c00eb16864ff9925152ac5e9c69ea18e637
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2025-09-26 12:54:33 -0400

    Update excercise_6.py

--- Lists/excercise_6.py
+++ Lists/excercise_6.py
@@ -1,15 +1,4 @@
-# Replace all occurence of 20 with 200
-list1 = [5, 10, 15, 20, 25, 50, 20]
-list1 = list(map(lambda x: 200 if x == 20 else x, list1))
-print(list1)
-#Method II
-list1 = [5, 10, 15, 20, 25, 50, 20]
 
-for i in range(len(list1)):
-    if list1[i] == 20:
-        list1[i] = 200
-
-print(list1)
 
 #Write function to flatten the nested list
 def flatten_nested_list(lst):
