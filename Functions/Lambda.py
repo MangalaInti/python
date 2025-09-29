4b1ab4e04f1e943e07f2ee847b6d85c5faaa36f7
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2025-09-29 12:08:16 -0400

    Update Lambda.py

--- Functions/Lambda.py
+++ Functions/Lambda.py
@@ -17,4 +17,24 @@ print(res(50,100))
 val = lambda i : i*i
 print(val(9))
 
+**Map funcion
+Map function takes two argments
+map(function, iterable) 
+   - Function will be applied to each item in iterable
+   - Iterable is like list, tuple etc
+
+#Square of each item
+numbers = [1,2,3,4]
+res = list(map(lambda x: x**2, numbers))
+print(res)
+
+#custom function
+def capitalize(word):
+  return word.upper()
+words = ['hello', 'world']
+
+res = map(capitalize, words)
+print(list(res))
+
+
 
