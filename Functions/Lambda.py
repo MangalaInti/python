9c0d411de5ab1e2840ed63a82428362560f85755
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2025-09-29 11:20:04 -0400

    Update Lambda.py

--- Functions/Lambda.py
+++ Functions/Lambda.py
@@ -1,1 +1,20 @@
-Lambda function is an anonymous function. Function can have any number of parameters in one statment
+Lambda function is an anonymous function i.e without any name. Function can have any number of parameters in one statment
+syntax : lambda argruments : expression
+
+#Multiply with a number
+val = lambda i: i*2
+print(val(25))
+
+#Multiply 3 arguments
+val = lambda x,y,z : x*y*z
+print(val(20,2,1))
+
+# Maximum of two numbers using lambda
+res = lambda x,y : x if (x >y)  else y
+print(res(50,100)) 
+
+# square of number using lambda
+val = lambda i : i*i
+print(val(9))
+
+
