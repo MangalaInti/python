edcd3ac02fa81eaf13435010c9bc10ef74393560
MangalaInti <148904906+MangalaInti@users.noreply.github.com>
2025-09-29 11:40:03 -0400

    Create Decorators.py

---
+++ Functions/Decorators.py
@@ -0,0 +1,19 @@
+Decorators are add-ons to the basic function which allows you to decoratr or enchance features of existing function
+without changing the actual code.
+Decorators are used to enchance the functionaities of existing function or a class
+It is a special type of function which takes function as argument and returns a function
+It defines inner function often referred as a wrapper function, which is used for add-ons to the orginal function
+
+--Types of Decorators
+1. Built-in decorator
+2. Chained Decorator
+3. Class Decorator
+
+Python provides several built-in decorators such as the most common one's are
+@property
+@classmethod
+@staticmethod
+@abstractmethod
+@lru_cache
+@singledispatch
+
